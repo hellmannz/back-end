6924bd43afceda67a3ed32d8f6deee57fc6c7edd
hellmannz <162449863+hellmannz@users.noreply.github.com>
2025-02-25 10:57:01 -0300

    boolean.js

---
+++ boolean.js
@@ -0,0 +1,15 @@
+// Operadores de comparação
+console.log("1" == 1)
+console.log(true == 1)
+console.log(false == 0)
+
+//Operador de igualdade estrita
+// (compara o valor e tipo)
+console.log("1" === 1)
+console.log(true === 1)
+console.log(false === 0)
+
+// Verificar tipo da variável
+console.log(typeof ("1")) // -> texto
+console.log(typeof(1)) // -> Number
+console.log(typeof(true)) // -> boolean
