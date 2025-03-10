2c43951fcf5e8ffbb8b064b9bb6e4bedf3f653ae
hellmannz <162449863+hellmannz@users.noreply.github.com>
2025-03-10 08:25:33 -0300

    exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -1,4 +1,12 @@
-let av1 = 10.0
-let av2 = 7.0
-let av3 = 4.0
+const av1 = 10.0
+const av2 = 7.0
+const av3 = 4.0
+const media = (av1+av2+av3)/3
+
+if (media>= 6.0){
+    console.log("Aprovado com media 7.0");
+}else{
+    console.log("Reprovado");
+}
+
 
